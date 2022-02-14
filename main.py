9a60661c7797e21e36a16deb3ef88fe97171e8fe
Shxl2 <pl238412@ahschool.com>
2022-02-13 20:44:24 -0500

    Added algorithm to play videos

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import cv2
+
+video = cv2.VideoCapture("RollingCan.mov")
+
+if not video.isOpened():
+    print("Error opening video  file")
+
+while video.isOpened():
+    ret, frame = video.read()
+
+    if ret:
+        cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+video.release()
+
+cv2.destroyAllWindows()
