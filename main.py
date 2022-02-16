d397a78242286635709f1ca419959ba59caa149c
Shxl2 <92115922+Shxl2@users.noreply.github.com>
2022-02-16 11:45:41 -0500

    Added split screen input and output

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import cv2
 import numpy as np
 video = cv2.VideoCapture("RollingCan.mov")
-counter = 0
+detectedFrames = 0
+totalFrames = 0
+
 if not video.isOpened():
     print("Error opening video  file")
 
 while video.isOpened():
-    ret, frameIn = video.read()
+    ret, frameOut = video.read()
+    totalFrames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
     if ret:
-        frameOut = frameIn
-        frameGray = cv2.medianBlur(frameIn, 5)
+
+        frameGray = cv2.medianBlur(frameOut, 5)
         frameGray = cv2.cvtColor(frameGray, cv2.COLOR_BGR2GRAY)
 
         rows = frameGray.shape[0]
-        circles = cv2.HoughCircles(frameGray, cv2.HOUGH_GRADIENT, 1, 100000000, param1 = 150, param2 = 15, minRadius = 80, maxRadius =90)
+        circles = cv2.HoughCircles(frameGray, cv2.HOUGH_GRADIENT, 1, 100000000, param1 = 150, param2 = 15, minRadius = 80, maxRadius = 90)
 
         if circles is not None:
-            counter += 1
+            detectedFrames += 1
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
                 center = (i[0], i[1])
@@ -27,9 +30,17 @@ while video.isOpened():
                 radius = i[2]
                 cv2.circle(frameOut, center, radius, (255, 0, 255), 3)
 
-        frameOut = cv2.putText(frameOut, "{} frames detected".format(counter), (600, 300), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 0, 0), 2, cv2.LINE_AA)
 
-        cv2.imshow('Frame', frameOut)
+
+        frameOut = cv2.putText(frameOut, "{}/{} frames detected".format(detectedFrames, totalFrames), (600, 300), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 0, 0), 2, cv2.LINE_AA)
+
+        ret, frameIn = video.read()
+        try:
+            Hori = np.concatenate((frameIn, frameOut), axis=1)
+        except ValueError:
+            break
+
+        cv2.imshow("Hi", Hori)
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
