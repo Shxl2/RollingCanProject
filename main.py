eca5e915b1aeaf04b7eb7397b47355692c98b5a0
Shxl2 <pl238412@ahschool.com>
2022-02-18 21:47:45 -0500

    Added video saving + in/out indication

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import cv2
 import numpy as np
 video = cv2.VideoCapture("RollingCan.mov")
-result = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*"mp4v"), 60, (640, 480))
+result = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*"mp4v"), 20.0, (3840, 1080))
 detectedFrames = 0
-totalFrames = 0
 
 if not video.isOpened():
     print("Error opening video  file")
 
 while video.isOpened():
     ret, frameOut = video.read()
+    ret, frameIn = video.read()
     totalFrames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_width = int(video.get(3))
-    frame_height = int(video.get(4))
-
-    size = (frame_width, frame_height)
 
     if ret:
-
         frameGray = cv2.medianBlur(frameOut, 5)
         frameGray = cv2.cvtColor(frameGray, cv2.COLOR_BGR2GRAY)
 
@@ -30,21 +25,26 @@ while video.isOpened():
             for i in circles[0, :]:
                 center = (i[0], i[1])
                 # circle center
-                cv2.circle(frameOut, center, 1, (0, 100, 100), 3)
+                cv2.circle(frameOut, center, 10, (0, 0, 0), -1)
                 # circle outline
                 radius = i[2]
-                cv2.circle(frameOut, center, radius, (255, 0, 255), 3)
+                cv2.circle(frameOut, center, radius, (0, 255, 0), 3)
 
         frameOut = cv2.putText(frameOut, "{}/{} frames detected".format(detectedFrames, totalFrames), (600, 300), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 0, 0), 2, cv2.LINE_AA)
+        frameOut = cv2.putText(frameOut, "Output", (900, 100), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 0, 0), 2,
+                               cv2.LINE_AA)
 
-        ret, frameIn = video.read()
+        frameIn = cv2.putText(frameIn, "Input", (900, 100), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 0, 0), 2,
+                              cv2.LINE_AA)
+
         try:
-            Hori = np.concatenate((frameIn, frameOut), axis=1)
+            combinedImage = np.concatenate((frameIn, frameOut), axis=1)
         except ValueError:
             break
 
-        result.write(Hori)
-        cv2.imshow("Hi", Hori)
+        result.write(combinedImage)
+
+        cv2.imshow("RollingCanProject", combinedImage)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
